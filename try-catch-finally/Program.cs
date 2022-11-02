ef8578e37c2e8a6b1df30b5d27f53c583b91f058
aisatzun <116556971+aisatzun@users.noreply.github.com>
2022-11-02 14:45:20 +0300

    final commit
    try catch finally

---
+++ try-catch-finally/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try 
+{
+Console.WriteLine("Bir sayı giriniz:");
+int s1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Girmiş olduğunuz sayı:"+s1);
+}
+catch (Exception ex)
+{
+  Console.WriteLine("Hata :"+ex.Message.ToString());
+}
+// finally 
+// {
+//    Console.Write("İşlem tamamlandı:");
+// }
+
+try
+{
+   // int a = int.Parse(null);
+   // int b = int.Parse("test");
+      int c = int.Parse("-20012305467");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine("Boş değer girdiniz");
+    Console.WriteLine(ex);    
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Veri tipi uygun değil");
+    Console.WriteLine(ex);
+}
+catch(OverflowException ex)
+{
+    Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz");
+    Console.WriteLine(ex);
+}
+
+finally{
+    Console.WriteLine("işlem tamamlandı");
+}
