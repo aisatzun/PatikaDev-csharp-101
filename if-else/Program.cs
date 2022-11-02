8d49b9b301753be1e979f5fdd9660234d695b8cc
aisatzun <116556971+aisatzun@users.noreply.github.com>
2022-11-02 17:08:45 +0300

    final commit

--- if-else/Program.cs
+++ if-else/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int time = DateTime.Now.Hour;
+
+if (time >= 6 && time < 11) 
+    Console.WriteLine("Günaydın!"); 
+ else if (time <= 18)
+    Console.WriteLine("İyi Günler!");
+ else  
+    Console.WriteLine("İyi Geceler!");
+
+ string sonuc = time<=18 ? "Merhaba, İyi Günler!" : "Merhaba, İyi Geceler!";
+
+sonuc = time >= 6 && time < 11 ? "Günaydın 2" : time <= 18 ? "İyi Günler 2" : "İyi Geceler 2";
+
+Console.WriteLine(sonuc);
+
+
