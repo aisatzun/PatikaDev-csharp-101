8cbf24f09e74111a69d4174be50e12c4031cbb93
aisatzun <116556971+aisatzun@users.noreply.github.com>
2022-11-05 14:16:21 +0300

    final commit
    while,foreach

--- while-foreach/Program.cs
+++ while-foreach/Program.cs
@@ -1,4 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 //--- while
+// Console.WriteLine("Sayı giriniz");
+// int sayi=int.Parse(Console.ReadLine());
 
+// int sayac=1;
+// int toplam=0;
+
+// while(sayac<=sayi)
+// {
+//     toplam +=sayac;
+//     sayac++;
+// }
+// Console.WriteLine(toplam/sayi);
+
+// // a dan z ye
+
+// char character='a';
+// while(character < 'z')
+// {
+//     Console.Write(character+" ");
+//     character++;
+// }
+
+//--- foreach
+Console.WriteLine("***  foreach  ***");
+
+string[] arabalar={"Bwm,Ford,Toyota,Nissan"};
+
+foreach(var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
