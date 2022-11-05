a234a0186218d4f3106b74d649867cc6fed1a483
aisatzun <116556971+aisatzun@users.noreply.github.com>
2022-11-05 14:57:12 +0300

    final commit
    array

--- array/Program.cs
+++ array/Program.cs
@@ -4,5 +4,35 @@
 
 string[] renkler = new string[5];
 
+string[] hayvanlar = {"Kedi","Kopek","Kuş","Maymun"};
+int[] dizi;
+dizi=new int[5];
 
-Console.WriteLine("Hello, World!");
+//--- Diziler değer atama ve erişim
+renkler[0]="Mavi";
+dizi[3]=10;
+
+Console.WriteLine(hayvanlar[1]);
+Console.WriteLine(dizi[3]);
+Console.WriteLine(renkler[0]);
+
+// Döngülerle dizi kullanımı
+// Klavyeden girilen n tane sayının ortalamasını hesaplayan program
+
+Console.WriteLine("Dizinin elaman sayısını giriniz");
+int diziUzunluk = int.Parse(Console.ReadLine());
+int[] sayiDizisi = new int[diziUzunluk];
+
+for(int i=0;i<diziUzunluk;i++)
+{
+    Console.Write("Lütfen {0}. sayıyı giriniz: ",i+1);
+    sayiDizisi[i] =int.Parse(Console.ReadLine());
+}
+
+int toplam=0;
+foreach ( var sayi in sayiDizisi)
+{
+    toplam+=sayi;
+
+    Console.WriteLine("Ortalama :"+ toplam/diziUzunluk);
+}
